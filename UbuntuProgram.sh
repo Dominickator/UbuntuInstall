27e2cb076b8f06738e3ded8489ba5c8ae5b2004f
Dominickator <92421959+Dominickator@users.noreply.github.com>
2022-10-23 15:18:20 -0500

    Update UbuntuProgram.sh

--- UbuntuProgram.sh
+++ UbuntuProgram.sh
@@ -3,6 +3,8 @@ sudo ubuntu-drivers install
 sudo apt update && sudo apt upgrade
 sudo apt install git -y
 sudo apt install tlp -y
+sudo apt install valgrind -y
+sudo apt install gdb -y
 sudo systemctl enable tlp.service
 sudo apt update && sudo apt upgrade
 sudo reboot
