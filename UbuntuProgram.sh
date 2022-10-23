5d5a41d00f7618634a4523ae4496f23e01d43ee0
Dominick Dickerson <dominickator122102@gmail.com>
2022-10-23 11:15:32 -0500

    Code Uploaded

---
+++ UbuntuProgram.sh
@@ -0,0 +1,8 @@
+sudo snap install code
+sudo ubuntu-drivers install
+sudo apt update && sudo apt upgrade
+sudo apt install git -y
+sudo apt install tlp -y
+sudo systemctl enable tlp.service
+sudo apt update && sudo apt upgrade
+sudo reboot
