7ba041171e666e6ed75ec462567ac2128c2c3b03
Dominickator <92421959+Dominickator@users.noreply.github.com>
2022-10-24 16:19:32 -0500

    Update UbuntuProgram.sh

--- UbuntuProgram.sh
+++ UbuntuProgram.sh
@@ -1,14 +1,15 @@
 sudo snap install code --classic
 sudo snap install discord
-sudo snap install steam
 sudo ubuntu-drivers autoinstall
 sudo apt update && sudo apt dist-upgrade
+sudo apt install steam -y
 sudo apt install git -y
 sudo apt install tlp -y
 sudo apt install valgrind -y
 sudo apt install gdb -y
 sudo apt install flatpak -y
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install flathub net.davidotek.pupgui2
 sudo systemctl enable tlp.service
 sudo apt update && sudo apt dist-upgrade
 sudo reboot
