03d6ec6de162e25e837db81ad9fd4b1befe515cc
Dominickator <92421959+Dominickator@users.noreply.github.com>
2022-10-23 20:16:22 -0500

    Update UbuntuProgram.sh

--- UbuntuProgram.sh
+++ UbuntuProgram.sh
@@ -5,6 +5,8 @@ sudo apt install git -y
 sudo apt install tlp -y
 sudo apt install valgrind -y
 sudo apt install gdb -y
+sudo apt install flatpak -y
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 sudo systemctl enable tlp.service
 sudo apt update && sudo apt dist-upgrade
 sudo reboot
