55b0d070cb007694e23b8f4ebdbfdbcd4e934537
Dickerson, Dominick <92421959+Dominickator@users.noreply.github.com>
2023-07-25 13:03:23 -0500

    Update UbuntuProgram.sh

--- UbuntuProgram.sh
+++ UbuntuProgram.sh
@@ -1,15 +1,18 @@
 sudo snap install code --classic
 sudo snap install discord
-sudo ubuntu-drivers autoinstall
 sudo apt update && sudo apt dist-upgrade
 sudo apt install steam -y
 sudo apt install git -y
-sudo apt install tlp -y
 sudo apt install valgrind -y
 sudo apt install gdb -y
 sudo apt install flatpak -y
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak install flathub net.davidotek.pupgui2
-sudo systemctl enable tlp.service
+cd ~/Documents
+git clone https://github.com/medusalix/xone
+cd xone
+sudo ./install.sh --release
+sudo xone-get-firmware.sh
+sudo apt install gnome-tweaks -y
+sudo apt install gnome-console -y
 sudo apt update && sudo apt dist-upgrade
 sudo reboot
